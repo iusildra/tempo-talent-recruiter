a1d4ef57b6929c0e0ae3e436cb8985c130f1f5bc
Lucas Nouguier <lucas.nouguier@protonmail.com>
2023-10-14 23:26:07 +0200

    added db connection

--- src/main/java/com/tempotalent/api/controllers/CountryController.java
+++ src/main/java/com/tempotalent/api/controllers/CountryController.java
@@ -1,63 +1,34 @@
 package com.tempotalent.api.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import com.tempotalent.api.models.Country;
 import com.tempotalent.api.repositories.CountryRepository;
 
 import io.micrometer.common.lang.Nullable;
 
-@RestController
-@RequestMapping("/api/v1/countries")
+@Controller
 public class CountryController {
   @Autowired
-  private final CountryRepository countryRepository;
+  private final CountryRepository repository;
 
-  public CountryController(CountryRepository countryRepository) {
-    this.countryRepository = countryRepository;
+  public CountryController(CountryRepository repository) {
+    this.repository = repository;
   }
 
-  @GetMapping
-  public List<Country> index() {
-    // return countryRepository.findAll();
-    return Arrays.asList(new Country(1, "Australia"), new Country(2, "New Zealand"));
-  }
-
-  @Nullable
-  @GetMapping("{id}")
-  public Country get(@PathVariable int id) {
-    return countryRepository.findById(id).orElse(null);
-  }
-
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public Country create(Country country) {
-    return countryRepository.save(country);
+  @QueryMapping
+  public List<Country> countries() {
+    return repository.findAll();
   }
 
   @Nullable
-  @PutMapping("{id}")
-  public Country update(@PathVariable int id, Country country) {
-    if (countryRepository.findById(id).isEmpty() || country.id() != id)
-      return null;
-    else
-      return countryRepository.save(country);
-  }
-
-  @DeleteMapping("{id}")
-  public void delete(@PathVariable int id) {
-    countryRepository.deleteById(id);
+  @QueryMapping
+  public Country countryById(@Argument int id) {
+    return repository.findById(id).orElse(null);
   }
 }
